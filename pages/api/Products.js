5c19fa8a4ae135a0963d537a8e3a1f1fee460c96
ahmedDev32 <166875212+ahmedDev32@users.noreply.github.com>
2024-06-21 15:06:07 -0700

    Update Products.js

--- pages/api/Products.js
+++ pages/api/Products.js
@@ -2,15 +2,27 @@ import fs from "fs/promises"; // Use the promise-based version of fs
 import ProductsModel from "../../Models/PostProduct";
 
 let handler = async (req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', 'https://ecomerce-kappa.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // Handle preflight request
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     try {
+        // Uncomment the database fetch if needed
         // let getData_db = await ProductsModel.find({});
         // console.log(getData_db)
+        
         // Read the JSON file asynchronously
         let data = await fs.readFile("./Models/products.json", "utf-8");
         let Ordata = JSON.parse(data);
-        let parsePros = Ordata.reverse()
+        let parsePros = Ordata.reverse();
         
-        // Fetch data from the database
+        // Fetch data from the database if needed
         
         let seen = new Set();
         let parsePro = parsePros.filter((item) => {
@@ -26,7 +38,7 @@ let handler = async (req, res) => {
 
         return res.status(200).json({ parsePro });
     } catch (err) {
-        console.log(err)
+        console.log(err);
         return res.status(500).json({ success: false, msg: "Internal Error" });
     }
 };
