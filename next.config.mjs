df4f3711db4be2f27f8a6b5d6a407e3545e03dba
ahmedDev32 <166875212+ahmedDev32@users.noreply.github.com>
2024-06-21 15:46:50 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-// next.config.js
+// next.config.mjs
 
 const nextConfig = {
   reactStrictMode: false,
@@ -19,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
